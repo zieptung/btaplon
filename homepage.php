f6d6fed869cf9b183bf29e584d1d6411b274a594
Phạm Trọng Hiện <hienphamtm74@gmail.com>
2024-12-08 17:28:03 +0700

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -6,51 +6,51 @@
 <title>Quản lý điểm sinh viên đại học</title>
 
 <body>
-    <div class="header">
-        <span class="header-icon"><i class="fa-solid fa-circle-user"></i></span>
-        <span class="header-text">Trang chủ</span>
-    </div>
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <ul>
-            <li>
-                <a href="#" class="logo">
-                    <span class="icon"><i class="fa-solid fa-house"></i></span>
-                    <span class="text">Trang chủ</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-envelope"></i></span>
-                    <span class="text">Tin nhắn</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-bell"></i></span>
-                    <span class="text">Diễn đàn</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-circle-exclamation"></i></span>
-                    <span class="text">Thông tin sinh viên</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-table"></i></span>
-                    <span class="text">Bảng điểm sinh viên</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="icon"><i class="fa-solid fa-right-from-bracket"></i></span>
-                    <span class="text">Đăng xuất</span>
-                </a>
-            </li>
-        </ul>
-    </div>
+   <div class="header">
+      <span class="header-icon"><i class="fa-solid fa-circle-user"></i></span>
+      <span class="header-text">Trang chủ</span>
+   </div>
+   <!-- Sidebar -->
+   <div class="sidebar">
+      <ul>
+         <li>
+            <a href="#" class="logo">
+               <span class="icon"><i class="fa-solid fa-house"></i></span>
+               <span class="text">Trang chủ</span>
+            </a>
+         </li>
+         <li>
+            <a href="#">
+               <span class="icon"><i class="fa-solid fa-envelope"></i></span>
+               <span class="text">Tin nhắn</span>
+            </a>
+         </li>
+         <li>
+            <a href="#">
+               <span class="icon"><i class="fa-solid fa-bell"></i></span>
+               <span class="text">Diễn đàn</span>
+            </a>
+         </li>
+         <li>
+            <a href="#">
+               <span class="icon"><i class="fa-solid fa-circle-exclamation"></i></span>
+               <span class="text">Thông tin sinh viên</span>
+            </a>
+         </li>
+         <li>
+            <a href="#">
+               <span class="icon"><i class="fa-solid fa-table"></i></span>
+               <span class="text">Bảng điểm sinh viên</span>
+            </a>
+         </li>
+         <li>
+            <a href="#">
+               <span class="icon"><i class="fa-solid fa-right-from-bracket"></i></span>
+               <span class="text">Đăng xuất</span>
+            </a>
+         </li>
+      </ul>
+   </div>
 </body>
 
 </html>

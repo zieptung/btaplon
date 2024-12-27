c94b6f6f8c8d5971c092da24443ebb39167346bc
Phạm Trọng Hiện <hienphamtm74@gmail.com>
2024-12-27 22:36:28 +0700

    hi

--- home/teacher/manager_sv.php
+++ home/teacher/manager_sv.php
@@ -1,3 +1,12 @@
-<?php
-echo "Hello, world!";
+<?php 
+   session_start();
+   include_once "connectdb.php";
+   if (isset($_SESSION['user_id'])) {
+       $user_id = $_SESSION['user_id'];
+       $sql = "SELECT hoten FROM user WHERE ma = '$user_id'";
+       $result = mysqli_query($con, $sql);
+       if ($row = mysqli_fetch_assoc($result)) {
+           echo "<span class='header-username'>" . $row['hoten'] . "</span>";
+       }
+   }
 ?>

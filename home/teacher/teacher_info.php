5527f501939d0fb9f0c1dd53a45e26dcaf553dbd
Tùn g <108751100+zieptung@users.noreply.github.com>
2024-12-29 10:18:54 +0700

    new 2

--- home/teacher/teacher_info.php
+++ home/teacher/teacher_info.php
@@ -1,13 +1,23 @@
 <?php
 include_once "connectdb.php";
 session_start();
-$ma='';$hoten='';$ns='';$diachi='';$sdt='';$email='';$gioitinh='';$mamon='';$chucvu='';$hocvan='';$malop='';
+$ma = '';
+$hoten = '';
+$ns = '';
+$diachi = '';
+$sdt = '';
+$email = '';
+$gioitinh = '';
+$mamon = '';
+$chucvu = '';
+$hocvan = '';
+$malop = '';
 
-if(isset($_SESSION['user_id'])){
+if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
-    $sql = "SELECT * FROM giangvien WHERE ma = '$user_id'";
+    $sql = "SELECT * FROM giang_vien WHERE ma = '$user_id'";
     $result = mysqli_query($con, $sql);
-    if($row = mysqli_fetch_assoc($result)){
+    if ($row = mysqli_fetch_assoc($result)) {
         $ma = $row['ma'];
         $hoten = $row['hoten'];
         $ns = $row['ns'];
@@ -22,14 +32,10 @@ if(isset($_SESSION['user_id'])){
     }
 }
 
-$sql1 ="SELECT giangvien.hoten, user.hoten
-FROM giangvien
-INNER JOIN user ON giangvien.hoten = user.hoten";
-
-if(isset($_POST['btnluu'])) {
+if (isset($_POST['btnluu'])) {
     $hoten = $_POST['txthoten'];
     $ns = $_POST['txtns'];
-    $diachi = $_POST['txtdiachi']; 
+    $diachi = $_POST['txtdiachi'];
     $sdt = $_POST['txtsdt'];
     $email = $_POST['txtemail'];
     $gioitinh = $_POST['txtgioitinh'];
@@ -37,8 +43,9 @@ if(isset($_POST['btnluu'])) {
     $chucvu = $_POST['txtchucvu'];
     $hocvan = $_POST['txthocvan'];
     $malop = $_POST['txtmalop'];
-}
-$sql = "UPDATE giangvien SET 
+
+    $sql1 = "UPDATE giang_vien SET 
+        hoten = '$hoten',
         ns = '$ns',
         diachi = '$diachi',
         sdt = '$sdt',
@@ -49,8 +56,10 @@ $sql = "UPDATE giangvien SET
         hocvan = '$hocvan',
         malop = '$malop'
         WHERE ma = '$ma'";
-
-mysqli_multi_query($con, $sql);
+    $sql2 = "UPDATE user SET email = '$email' WHERE ma = '$user_id'";
+    if (mysqli_query($con, $sql1) && mysqli_query($con, $sql2)) {
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -70,7 +79,7 @@ mysqli_multi_query($con, $sql);
         <?php
         if (isset($_SESSION['user_id'])) {
             $user_id = $_SESSION['user_id'];
-            $sql = "SELECT hoten FROM giangvien WHERE ma = '$user_id'";
+            $sql = "SELECT hoten FROM giang_vien WHERE ma = '$user_id'";
             $result = mysqli_query($con, $sql);
             if ($row = mysqli_fetch_assoc($result)) {
                 echo "<span class='header-username'>" . $row['hoten'] . "</span>";
@@ -121,125 +130,146 @@ mysqli_multi_query($con, $sql);
     </div>
     <!-- content -->
     <article class="content">
-    <div class="container mt-4">
-        <form method="POST" action="">
-            <div class="form-group">
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtma" class="form-label me-3 mb-0" style="min-width: 100px;">Mã giảng viên</label>
-                            <input type="text" class="form-control" name="txtma" placeholder="Nhập mã" value="<?php echo  $ma; ?>" disabled>
+        <div class="container mt-4">
+            <form method="POST" action="">
+                <div class="form-group">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtma" class="form-label me-3 mb-0" style="min-width: 100px;">Mã giảng
+                                    viên</label>
+                                <input type="text" class="form-control" name="txtma" placeholder="Nhập mã"
+                                    value="<?php echo $ma; ?>" disabled>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txthoten" class="form-label me-3 mb-0" style="min-width: 100px;">Họ và tên</label>
-                            <input type="text" class="form-control" name="txthoten" placeholder="Nhập họ tên" value="<?php echo $hoten; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txthoten" class="form-label me-3 mb-0" style="min-width: 100px;">Họ và
+                                    tên</label>
+                                <input type="text" class="form-control" name="txthoten" placeholder="Nhập họ tên"
+                                    value="<?php echo $hoten; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtns" class="form-label me-3 mb-0" style="min-width: 100px;">Ngày sinh</label>
-                            <input type="date" class="form-control" name="txtns" placeholder="Nhập ngày sinh" value="<?php echo $ns; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtns" class="form-label me-3 mb-0" style="min-width: 100px;">Ngày
+                                    sinh</label>
+                                <input type="date" class="form-control" name="txtns" placeholder="Nhập ngày sinh"
+                                    value="<?php echo $ns; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtdiachi" class="form-label me-3 mb-0" style="min-width: 100px;">Địa chỉ</label>
-                            <input type="text" class="form-control" name="txtdiachi" placeholder="Nhập địa chỉ" value="<?php echo $diachi; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtdiachi" class="form-label me-3 mb-0" style="min-width: 100px;">Địa
+                                    chỉ</label>
+                                <input type="text" class="form-control" name="txtdiachi" placeholder="Nhập địa chỉ"
+                                    value="<?php echo $diachi; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtsdt" class="form-label me-3 mb-0" style="min-width: 100px;">Số điện thoại</label>
-                            <input type="text" class="form-control" name="txtsdt" placeholder="Nhập số điện thoại" value="<?php echo $sdt; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtsdt" class="form-label me-3 mb-0" style="min-width: 100px;">Số điện
+                                    thoại</label>
+                                <input type="text" class="form-control" name="txtsdt" placeholder="Nhập số điện thoại"
+                                    value="<?php echo $sdt; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtemail" class="form-label me-3 mb-0" style="min-width: 100px;">Email</label>
-                            <input type="text" class="form-control" name="txtemail" placeholder="Nhập email" value="<?php echo $email; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtemail" class="form-label me-3 mb-0"
+                                    style="min-width: 100px;">Email</label>
+                                <input type="text" class="form-control" name="txtemail" placeholder="Nhập email"
+                                    value="<?php echo $email; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtgioitinh" class="form-label me-3 mb-0" style="min-width: 100px;">Giới tính</label>
-                            <select class="form-select custom-select" name="txtgioitinh">
-                                <option value="Nam" <?php echo ($gioitinh == 'Nam') ? 'selected' : ''; ?>>Nam</option>
-                                <option value="Nữ" <?php echo ($gioitinh == 'Nữ') ? 'selected' : ''; ?>>Nữ</option>
-                            </select>
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtgioitinh" class="form-label me-3 mb-0" style="min-width: 100px;">Giới
+                                    tính</label>
+                                <select class="form-select custom-select" name="txtgioitinh">
+                                    <option value="Nam" <?php echo ($gioitinh == 'Nam') ? 'selected' : ''; ?>>Nam</option>
+                                    <option value="Nữ" <?php echo ($gioitinh == 'Nữ') ? 'selected' : ''; ?>>Nữ</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtmamon" class="form-label me-3 mb-0" style="min-width: 100px;">Mã môn</label>
-                            <input type="text" class="form-control" name="txtmamon" placeholder="Nhập mã môn" value="<?php echo $mamon; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtmamon" class="form-label me-3 mb-0" style="min-width: 100px;">Mã
+                                    môn</label>
+                                <input type="text" class="form-control" name="txtmamon" placeholder="Nhập mã môn"
+                                    value="<?php echo $mamon; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtchucvu" class="form-label me-3 mb-0" style="min-width: 100px;">Chức vụ</label>
-                            <input type="text" class="form-control" name="txtchucvu" placeholder="Nhập chức vụ" value="<?php echo $chucvu; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtchucvu" class="form-label me-3 mb-0" style="min-width: 100px;">Chức
+                                    vụ</label>
+                                <input type="text" class="form-control" name="txtchucvu" placeholder="Nhập chức vụ"
+                                    value="<?php echo $chucvu; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtchucvu" class="form-label me-3 mb-0" style="min-width: 100px;">Học vấn</label>
-                            <input type="text" class="form-control" name="txthocvan" placeholder="Nhập học vấn" value="<?php echo $hocvan; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtchucvu" class="form-label me-3 mb-0" style="min-width: 100px;">Học
+                                    vấn</label>
+                                <input type="text" class="form-control" name="txthocvan" placeholder="Nhập học vấn"
+                                    value="<?php echo $hocvan; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <div class="d-flex align-items-center">
-                            <label for="txtchucvu" class="form-label me-3 mb-0" style="min-width: 100px;">Mã lớp</label>
-                            <input type="text" class="form-control" name="txtmalop" placeholder="Nhập mã lớp" value="<?php echo $malop; ?>">
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <div class="d-flex align-items-center">
+                                <label for="txtchucvu" class="form-label me-3 mb-0" style="min-width: 100px;">Mã
+                                    lớp</label>
+                                <input type="text" class="form-control" name="txtmalop" placeholder="Nhập mã lớp"
+                                    value="<?php echo $malop; ?>">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="form-group row mb-3">
-                    <div class="col-md-6">
-                        <button type="submit" name="btnluu" class="btn btn-success me-2 hidden">Lưu</button>
+                    <div class="form-group row mb-3">
+                        <div class="col-md-6">
+                            <button type="submit" name="btnluu" class="btn btn-success me-2 hidden">Lưu</button>
+                        </div>
                     </div>
-                </div>
-                <script>
-                    document.addEventListener('DOMContentLoaded', function() {
-                        const inputs = document.querySelectorAll('input, select');
-                        const saveButton = document.querySelector('button[name="btnluu"]');
+                    <script>
+                        document.addEventListener('DOMContentLoaded', function () {
+                            const inputs = document.querySelectorAll('input, select');
+                            const saveButton = document.querySelector('button[name="btnluu"]');
 
-                        inputs.forEach(input => {
-                            input.addEventListener('input', function() {
-                                saveButton.classList.remove('hidden');
+                            inputs.forEach(input => {
+                                input.addEventListener('input', function () {
+                                    saveButton.classList.remove('hidden');
+                                });
                             });
                         });
-                    });
-                </script>
-                <style>
-                    .hidden {
-                        display: none;
-                    }
-                </style>
-            </div>
-        </form>
-    </div>
+                    </script>
+                    <style>
+                        .hidden {
+                            display: none;
+                        }
+                    </style>
+                </div>
+            </form>
+        </div>
     </article>
 </body>
 

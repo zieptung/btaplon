7e732f00900eeea8338db5fc253844e9465f0b7f
Tùn g <108751100+zieptung@users.noreply.github.com>
2024-12-29 18:19:00 +0700

    sad

--- home/teacher/teacher_board.php
+++ home/teacher/teacher_board.php
@@ -45,8 +45,6 @@ if (isset($_POST["btnnew"])) {
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="teacher_homepage.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <title>Quản lý điểm sinh viên đại học</title>
 
 <body>
